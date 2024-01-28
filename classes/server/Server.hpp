ffdc3d2a6b0d9617babe10b14297e6c5073f49d4
AhmedAlsikely <ahmed.alsikely773@gmail.com>
2024-01-28 22:22:37 +0200

    V1.2.0

--- classes/server/Server.hpp
+++ classes/server/Server.hpp
@@ -14,26 +14,18 @@
 
 class Server{
     public:
-    int socketDesc, newSocket;
-    ssize_t readSize;
-    struct sockaddr_in server, client;
-    char message[1024] = {0};
-
-    Std_Return server_Init(int port_number);
-    Std_Return server_Accept();
-    Std_Return server_Receive();
-    Std_Return server_Recv_Command();
-    // Std_Return server_Send(const char *data);
-    Std_Return server_Close();
-
-
+        Std_Return server_Init(int port_number);
+        Std_Return server_Accept();
+        Std_Return server_Receive();
+        Std_Return server_Recv_Command();
+        Std_Return server_Close();
 
     private:
-    const char *youtube = "firefox https://www.youtube.com";
-    const char *cal = "gnome-calculator";
-    const char *vscode = "code ~/codes";
-    const char *poweroff ="systemctl poweroff";
-    const char *terminal ="gnome-terminal";
+        int socketDesc, newSocket;
+        ssize_t readSize;
+        struct sockaddr_in server, client;
+        char message[1024] = {0};
+
 };
 
 #endif
